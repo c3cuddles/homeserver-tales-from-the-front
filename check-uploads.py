65fc6a9e2ca9f614d0244ee923ea651b35e6f740
Paige Weber <paigeweber31@gmail.com>
2025-07-18 04:05:34 -0500

    intial idea for script

---
+++ check-uploads.py
@@ -0,0 +1,7 @@
+#/usr/bin/python3
+# this script will check our markdown to make sure that it doesn't contain
+# sensitive information like passwords. Of course, every file should be
+# inspected by hand. This is only a secondary, last line of defense.
+
+# can we make git run this before each commit???
+
